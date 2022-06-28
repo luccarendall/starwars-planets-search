cb995718d23dee6e3a7c4b3300d9c9e6b6b7e81d
Lucca Rendall <luccarendall2@gmail.com>
2022-06-28 10:24:46 -0300

    Filtro de título funciona mas aplicação quebra no botão de filtros numericos

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,6 +2,12 @@ import React, { useEffect, useState } from 'react';
 
 function Table() {
   const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [titleFilter, setTitleFilter] = useState('');
+  const [filterType, setFilterType] = useState('population');
+  const [operator, setOperator] = useState('maior que');
+  const [value, setValue] = useState('0');
+  const [numericFilters, setNumericFilter] = useState([]);
 
   useEffect(() => {
     const fetchPlanets = async () => {
@@ -9,51 +15,153 @@ function Table() {
       const APIdata = await response.json();
       console.log(APIdata);
       setData(APIdata.results);
+      setFilteredData(APIdata.results);
     };
     fetchPlanets();
   }, []);
+
+  // O useEffect aq vai executar uma função (primeiro parâmetro(antes da virgula)) sempre que algo mudar. Nesse caso o que vai mudar é o titleFilter que recebe o valor que a gente coloca no input
+  useEffect(() => {
+    const filteredPlanets = data.filter(
+      (planets) => planets.name.toLowerCase().includes(titleFilter),
+    );
+
+    // aqui vamos percorrer o acumulador, que inicialmente pega o filteredPlanets, e vamos acumular os filtros numericos usados.
+    setFilteredData(numericFilters.reduce(
+      (acumulador, filter) => acumulador.filter((planets) => {
+        switch (filter.operator) {
+        case 'maior que':
+          return planets[filter.filterType] > Number(filter.value);
+        case 'menor que':
+          return planets[filter.filterType] < Number(filter.value);
+        case 'igual a':
+          return planets[filter.filterType] === Number(filter.value);
+        default:
+          return true;
+        }
+      }), filteredPlanets,
+    ));
+  }, [titleFilter, numericFilters, data]);
+
+  const handleTitleFilter = ({ target }) => {
+    setTitleFilter(target.value.toLowerCase());
+  };
+
+  const handleNumericFilter = () => {
+    const newNumericFilter = {
+      filterType,
+      operator,
+      value,
+    };
+    setNumericFilter([...numericFilters, newNumericFilter]);
+  };
+
+  const handleDeleteFilter = (index) => {
+    setNumericFilter(numericFilters.filter((_item, itemIndex) => itemIndex !== index));
+  };
+
   return (
-    <table border="1">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>URL</th>
-        </tr>
-      </thead>
+    <main>
+      <form>
+        <input
+          type="text"
+          placeholder="Search"
+          data-testid="name-filter"
+          onChange={ handleTitleFilter }
+        />
+        <label htmlFor="filter">
+          Filtrar por:
+          <select
+            id="filter"
+            data-testid="column-filter"
+            onChange={ ({ target }) => setFilterType(target.value) }
+          >
+            <option>population</option>
+            <option>orbital_period</option>
+            <option>diameter</option>
+            <option>rotation_period</option>
+            <option>surface_water</option>
+          </select>
+        </label>
 
-      <tbody>
-        {/* Lembrete: Se algum dos resultados fosse um array de objetos, usaria: {planets.films.map(films => name)} dentro do td, para com que cada objeto mostrasse apenas o nome do filme por exemplo */}
-        {data.map((planets) => (
-          <tr key={ planets.name }>
-            <td>{planets.name}</td>
-            <td>{planets.rotation_period}</td>
-            <td>{planets.orbital_period}</td>
-            <td>{planets.diameter}</td>
-            <td>{planets.climate}</td>
-            <td>{planets.gravity}</td>
-            <td>{planets.terrain}</td>
-            <td>{planets.surface_water}</td>
-            <td>{planets.population}</td>
-            <td>{planets.films}</td>
-            <td>{planets.created}</td>
-            <td>{planets.edited}</td>
-            <td>{planets.url}</td>
+        <label htmlFor="operator">
+          Operador:
+          <select
+            id="operator"
+            onChange={ ({ target }) => setOperator(target.value) }
+          >
+            <option>maior que</option>
+            <option>menor que</option>
+            <option>igual a</option>
+          </select>
+        </label>
+
+        <input
+          type="number"
+          placeholder="0"
+          onChange={ ({ target }) => setValue(target.value) }
+        />
+
+        <button
+          type="button"
+          onClick={ handleNumericFilter }
+        >
+          Filtrar
+        </button>
+      </form>
+
+      {numericFilters.map((filter, index) => (
+        <button
+          type="button"
+          data-testid="button-remove-filters"
+          key={ `${filter.filter.type}-${index}` }
+          onClick={ () => handleDeleteFilter(index) }
+        >
+          {`${filter.filterType} ${filter.operator} ${filter.value}`}
+        </button>))}
+
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
           </tr>
-        ))}
-      </tbody>
+        </thead>
 
-    </table>
+        <tbody>
+          {/* Lembrete: Se algum dos resultados fosse um array de objetos, usaria: {planets.films.map(films => name)} dentro do td, para com que cada objeto mostrasse apenas o nome do filme por exemplo */}
+          {filteredData.map((planets) => (
+            <tr key={ planets.name }>
+              <td>{planets.name}</td>
+              <td>{planets.rotation_period}</td>
+              <td>{planets.orbital_period}</td>
+              <td>{planets.diameter}</td>
+              <td>{planets.climate}</td>
+              <td>{planets.gravity}</td>
+              <td>{planets.terrain}</td>
+              <td>{planets.surface_water}</td>
+              <td>{planets.population}</td>
+              <td>{planets.films}</td>
+              <td>{planets.created}</td>
+              <td>{planets.edited}</td>
+              <td>{planets.url}</td>
+            </tr>
+          ))}
+        </tbody>
+
+      </table>
+    </main>
   );
 }
 
