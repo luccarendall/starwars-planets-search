081cfb4fcaaa2703aaee35d6889897f8d22624c4
Lucca Rendall <luccarendall2@gmail.com>
2022-06-29 12:27:55 -0300

    Passando funções e estados locais para o contexto

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,72 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext } from 'react';
+import myContext from '../context/myContext';
 
 function Table() {
-  const [data, setData] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
-  const [titleFilter, setTitleFilter] = useState('');
-  const [filterType, setFilterType] = useState('population');
-  const [operator, setOperator] = useState('maior que');
-  const [inputValue, setInputValue] = useState(0);
-  const [numericFilters, setNumericFilter] = useState([]);
-
-  useEffect(() => {
-    const fetchPlanets = async () => {
-      const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
-      const APIdata = await response.json();
-      console.log(APIdata);
-      setData(APIdata.results);
-      setFilteredData(APIdata.results);
-    };
-    fetchPlanets();
-  }, []);
-
-  // O useEffect aq vai executar uma função (primeiro parâmetro(antes da virgula)) sempre que algo mudar. Nesse caso o que vai mudar é o titleFilter que recebe o valor que a gente coloca no input
-  useEffect(() => {
-    const filteredPlanets = data.filter(
-      (planets) => planets.name.toLowerCase().includes(titleFilter),
-    );
-
-    // aqui vamos percorrer o acumulador, que inicialmente pega o filteredPlanets, e vamos acumular os filtros numericos usados.
-    setFilteredData(numericFilters.reduce(
-      (acumulador, filter) => acumulador.filter((planets) => {
-        switch (filter.operator) {
-        case 'maior que':
-          return planets[filter.filterType] > Number(filter.value);
-        case 'menor que':
-          return planets[filter.filterType] < Number(filter.value);
-        case 'igual a':
-          return planets[filter.filterType] === Number(filter.value);
-        default:
-          return true;
-        }
-      }), filteredPlanets,
-    ));
-  }, [titleFilter, numericFilters, data]);
-
-  const handleTitleFilter = ({ target }) => {
-    setTitleFilter(target.value.toLowerCase());
-  };
-
-  const handleNumericFilter = () => {
-    const newNumericFilter = [
-      filterType,
-      operator,
-      inputValue,
-    ];
-    setNumericFilter([...numericFilters, newNumericFilter]);
-  };
-
-  const handleDeleteFilter = (index) => {
-    setNumericFilter(numericFilters.filter((_item, itemIndex) => itemIndex !== index));
-  };
-
-  // const filterBtn = () => {
-  //   const filters = [column, operator, value];
-
-  //   setFilterValues([...filterValues, filters]);
-  //   const columnOptionsFilter = columnOptions.filter((filter) => filter !== column);
-  //   setColumnOptions(columnOptionsFilter);
-  // };
+  const context = useContext(myContext);
+  const {
+    filteredData,
+    inputValue,
+    numericFilters,
+    handleTitleFilter,
+    handleNumericFilter,
+    handleDeleteFilter,
+    handlerFilter,
+    handlerOperator,
+    handlerNumberInput,
+  } = context;
 
   return (
     <main>
@@ -82,7 +29,7 @@ function Table() {
           <select
             id="filter"
             data-testid="column-filter"
-            onChange={ ({ target }) => setFilterType(target.value) }
+            onChange={ handlerFilter }
           >
             <option>population</option>
             <option>orbital_period</option>
@@ -97,7 +44,7 @@ function Table() {
           <select
             id="operator"
             data-testid="comparison-filter"
-            onChange={ ({ target }) => setOperator(target.value) }
+            onChange={ handlerOperator }
           >
             <option>maior que</option>
             <option>menor que</option>
@@ -110,7 +57,7 @@ function Table() {
           placeholder="0"
           value={ inputValue }
           data-testid="value-filter"
-          onChange={ ({ target }) => setInputValue(target.value) }
+          onChange={ handlerNumberInput }
         />
 
         {/* BOTÃO DE FILTRAR */}
--- src/context/myProvider.js
+++ src/context/myProvider.js
@@ -1,24 +1,107 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import MyContext from './myContext';
-// import fetchPlanets from './service/FetchAPI';
 
 function Provider({ children }) {
-  const INITIAL_STATE = {
-    nome: 'Chewbacca',
-    altura: 2.15,
-    fraseFavorita: 'RRRAARRWHHGWWR',
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [titleFilter, setTitleFilter] = useState('');
+  const [filterType, setFilterType] = useState('population');
+  const [operator, setOperator] = useState('maior que');
+  const [inputValue, setInputValue] = useState(0);
+  const [numericFilters, setNumericFilter] = useState([]);
+  const [filterOptions, setFilterOptions] = useState([
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ]);
+
+  // Fetch da API, pegando a chave results
+  useEffect(() => {
+    const fetchPlanets = async () => {
+      const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+      const APIdata = await response.json();
+      console.log(APIdata.results);
+      setData(APIdata.results);
+      setFilteredData(APIdata.results);
+    };
+    fetchPlanets();
+  }, []);
+
+  // O useEffect aq vai executar uma função (primeiro parâmetro(antes da virgula)) sempre que algo mudar. Nesse caso o que vai mudar é o titleFilter que recebe o valor que a gente coloca no input
+  useEffect(() => {
+    const filteredPlanets = data.filter(
+      (planets) => planets.name.toLowerCase().includes(titleFilter),
+    );
+
+    // aqui vamos percorrer o acumulador, que inicialmente pega o filteredPlanets, e vamos acumular os filtros numericos usados.
+    setFilteredData(numericFilters.reduce(
+      (acumulador, filter) => acumulador.filter((planets) => {
+        switch (filter.operator) {
+        case 'maior que':
+          return planets[filter.filterType] > Number(filter.value);
+        case 'menor que':
+          return planets[filter.filterType] < Number(filter.value);
+        case 'igual a':
+          return planets[filter.filterType] === Number(filter.value);
+        default:
+          return true;
+        }
+      }), filteredPlanets,
+    ));
+  }, [titleFilter, numericFilters, data]);
+
+  const handleTitleFilter = ({ target }) => {
+    setTitleFilter(target.value.toLowerCase());
+  // console.log('handleTitleFilter funcionou');
   };
 
-  const [state, setState] = useState(INITIAL_STATE);
+  // handle para controlar o botão de filtragem
+  const handleNumericFilter = () => {
+    const newNumericFilter = [filterType, operator, inputValue];
+    setNumericFilter([...numericFilters, newNumericFilter]);
+    const selectedFilter = filterOptions.filter((filter) => filter !== filterType);
+    setFilterOptions(selectedFilter);
+
+    console.log('handleNumericFilter funcionou');
+  };
+
+  // Botão de deletar filtros
+  const handleDeleteFilter = (index) => {
+    setNumericFilter(numericFilters.filter((_item, itemIndex) => itemIndex !== index));
+  // console.log('handleDeleteFilter funcionou');
+  };
+
+  const handlerFilter = ({ target }) => {
+    setFilterType(target.value);
+  };
+
+  const handlerOperator = ({ target }) => {
+    setOperator(target.value);
+  };
+
+  const handlerNumberInput = ({ target }) => {
+    setInputValue(target.value);
+  };
 
   const contextValue = {
-    state,
-    setState,
-    // fetchPlanets,
+    data,
+    filteredData,
+    titleFilter,
+    filterType,
+    operator,
+    inputValue,
+    numericFilters,
+    filterOptions,
+    handleTitleFilter,
+    handleNumericFilter,
+    handleDeleteFilter,
+    handlerFilter,
+    handlerOperator,
+    handlerNumberInput,
   };
-
-  console.log(contextValue);
 
   return (
     <MyContext.Provider value={ contextValue }>
